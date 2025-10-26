19eebb1a02d616d952fa08b4cf6dfb71cf48d7dd
Aditya Patra <adityabpatra@gmail.com>
2025-10-26 14:41:18 -0700

    input work, prepping for tets

--- main.cpp
+++ main.cpp
@@ -7,10 +7,17 @@ int main() {
     string code_lst[20000];
     string curr_code;
     ifstream file("codes.txt");
+
+    int reading[3];
+    int sorting[3];
+    int inserting[3];
+    int deleting[3];
+    
     for(int i = 0; getline(file, curr_code); i++) {
         code_lst[i] = curr_code;
-        cout << code_lst[i];
+        cout << code_lst[i] << endl;
     }
+
     return 0;
 }
 
